d0f705bc6bd0a7300440206fa494fabe1c3da368
aya-matroud <94105930+aya-matroud@users.noreply.github.com>
2023-04-10 07:43:51 -0700

    Aya (#5)
    * test
    
    * addBasics
    
    * addCompany
    
    * fixed
    
    * addBranchAndShift
    
    * fixed

--- app/Models/Fingerprint.php
+++ app/Models/Fingerprint.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Company extends Model
+class Fingerprint extends Model
 {
     use HasFactory;
+
     protected $guarded=[];
 }
