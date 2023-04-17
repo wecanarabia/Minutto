baecdfe0d4ca899953e02a0c72d4359ba6adfec2
mina <mina899432@gmail.com>
2023-04-17 12:27:04 +0200

    trans

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-// use Spatie\Translatable\HasTranslations;
+use Spatie\Translatable\HasTranslations;
 
 
 class Company extends Model
