bab6f90fefc31764f04daaae4fa45df55602ec61
mina <mina899432@gmail.com>
2023-05-30 23:46:17 +0300

    tables

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -26,7 +26,7 @@
         ||$('table').hasClass('table-vacations')||$('table').hasClass('table-vacations2')
         ||$('table').hasClass('table-salaries')||$('table').hasClass('table-logs')) {
 
-            $('table:first')
+            $('table')
             .addClass( 'nowrap' )
         .dataTable( {
             responsive: true,
@@ -35,15 +35,15 @@
                 { targets: [-1, -3], className: 'dt-body-right' }
             ]
         });
-            $('.table-leaves, .table-rewards, .table-advances, .table-extra, .table-alerts, .table-vacations, .table-vacations2, .table-salaries, .table-logs')
-        .addClass( 'nowrap' )
-        .dataTable( {
-            responsive: true,
-            order:[[0, 'desc']],
-            columnDefs: [
-                { targets: [-1, -3], className: 'dt-body-right' }
-            ]
-        });
+        //     $('.table-leaves, .table-rewards, .table-advances, .table-extra, .table-alerts, .table-vacations, .table-vacations2, .table-salaries, .table-logs')
+        // .addClass( 'nowrap' )
+        // .dataTable( {
+        //     responsive: true,
+        //     order:[[0, 'desc']],
+        //     columnDefs: [
+        //         { targets: [-1, -3], className: 'dt-body-right' }
+        //     ]
+        // });
 
         }else{
 
