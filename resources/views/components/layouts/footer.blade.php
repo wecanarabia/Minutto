ad87b71366011c4e4d483208b1e44a49ef2141dd
mina <mina899432@gmail.com>
2023-05-30 23:42:58 +0300

    tables

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -26,7 +26,7 @@
         ||$('table').hasClass('table-vacations')||$('table').hasClass('table-vacations2')
         ||$('table').hasClass('table-salaries')||$('table').hasClass('table-logs')) {
 
-            $('.table-attendance')
+            $('table:first')
             .addClass( 'nowrap' )
         .dataTable( {
             responsive: true,
@@ -35,7 +35,7 @@
                 { targets: [-1, -3], className: 'dt-body-right' }
             ]
         });
-            $(' .table-leaves, .table-rewards, .table-advances, .table-extra, .table-alerts, .table-vacations, .table-vacations2, .table-salaries, .table-logs')
+            $('.table-leaves, .table-rewards, .table-advances, .table-extra, .table-alerts, .table-vacations, .table-vacations2, .table-salaries, .table-logs')
         .addClass( 'nowrap' )
         .dataTable( {
             responsive: true,
