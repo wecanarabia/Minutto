bc2ab9cfb4e50534c1720bf136324cf296e84608
aya-matroud <ayamatroud8@gmail.com>
2023-06-06 02:15:27 -0700

    editLeave

--- app/Http/Resources/LeaveResource.php
+++ app/Http/Resources/LeaveResource.php
@@ -20,6 +20,8 @@ class LeaveResource extends JsonResource
             'leave_date'=>$this->leave_date,
             'from'=>$this->from,
             'to'=>$this->to,
+            'time_leave'=>$this->time_leave,
+            'time_back'=>$this->time_back,
             'note'=>$this->note,
             'file'=>$this->file,
             'status'=>$this->status,
