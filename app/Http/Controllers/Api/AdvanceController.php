d666d891db7645ef7af6bf95eb5e900a56e6e5a7
aya-matroud <ayamatroud8@gmail.com>
2023-06-08 05:02:51 -0700

    addAdvance

--- app/Http/Controllers/Api/AdvanceController.php
+++ app/Http/Controllers/Api/AdvanceController.php
@@ -10,6 +10,7 @@ use App\Repositories\Repository;
 use App\Http\Resources\AdvanceResource;
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class AdvanceController extends ApiController
 {
@@ -38,6 +39,31 @@ class AdvanceController extends ApiController
         $advances = Auth::user()->advances;
         return $this->returnData('data',  AdvanceResource::collection( $advances ), __('Get  succesfully'));
 
+    }
+
+    public function getCountersForAdvances()
+    {
+
+        $currentM=Carbon::today()->format('m');
+        $currentY=Carbon::today()->format('Y');
+
+
+        $data = array();
+
+        $percentege=User::find(Auth::user()->id)->branch->company->advances_percentage;
+        // $data['total'] = Carbon::parse($leave)->hour;
+        $data['percentage'] = $percentege;
+
+
+        $data['total'] =User::find(Auth::user()->id)->branch->company->advanes_count;
+
+        $data['taken'] = Advance::where('user_id', Auth::user()->id)
+        ->whereMonth('created_at', $currentM)
+        ->whereYear('created_at', $currentY)
+        ->get()->count();
+
+
+        return $this->returnData('data', $data, __('Succesfully'));
     }
 
 }
