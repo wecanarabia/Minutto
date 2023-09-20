e2f41efb07416017ae8973f597836c3482a503ad
mina <mina899432@gmail.com>
2023-09-20 15:21:49 +0300

    new

--- resources/views/components/front-layouts/body-header.blade.php
+++ resources/views/components/front-layouts/body-header.blade.php
@@ -283,7 +283,7 @@
               <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" data-bs-toggle="dropdown" aria-expanded="false">
                 <div class="d-flex align-items-center">
                   <div class="user-profile-img">
-                    <img src="../../dist/images/profile/user-1.jpg" class="rounded-circle" width="35" height="35" alt="" />
+                    <img src="{{ Auth::user()->image }}" class="rounded-circle" width="35" height="35" alt="" />
                   </div>
                 </div>
               </a>
