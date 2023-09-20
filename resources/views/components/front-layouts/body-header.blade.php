66566c7e27f704da2d13316fb1acf496338de5d2
mina <mina899432@gmail.com>
2023-09-20 15:15:43 +0300

    new

--- resources/views/components/front-layouts/body-header.blade.php
+++ resources/views/components/front-layouts/body-header.blade.php
@@ -293,12 +293,12 @@
                     <h5 class="mb-0 fs-5 fw-semibold">User Profile</h5>
                   </div>
                   <div class="d-flex align-items-center py-9 mx-7 border-bottom">
-                    <img src="../../dist/images/profile/user-1.jpg" class="rounded-circle" width="80" height="80" alt="" />
+                    <img src="{{ Auth::user()->image }}" class="rounded-circle" width="80" height="80" alt="" />
                     <div class="ms-3">
-                      <h5 class="mb-1 fs-3">Mathew Anderson</h5>
-                      <span class="mb-1 d-block text-dark">Designer</span>
+                      <h5 class="mb-1 fs-3">{{ Auth::user()->name }}</h5>
+                      <span class="mb-1 d-block text-dark">{{ Auth::user()->role->name }}</span>
                       <p class="mb-0 d-flex text-dark align-items-center gap-2">
-                        <i class="ti ti-mail fs-4"></i> info@modernize.com
+                        <i class="ti ti-mail fs-4"></i> {{ Auth::user()->email }}
                       </p>
                     </div>
                   </div>
@@ -312,40 +312,8 @@
                         <span class="d-block text-dark">Account Settings</span>
                       </div>
                     </a>
-                    <a href="app-email.html" class="py-8 px-7 d-flex align-items-center">
-                      <span class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
-                        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-inbox.svg" alt="" width="24" height="24">
-                      </span>
-                      <div class="w-75 d-inline-block v-middle ps-3">
-                        <h6 class="mb-1 bg-hover-primary fw-semibold">My Inbox</h6>
-                        <span class="d-block text-dark">Messages & Emails</span>
-                      </div>
-                    </a>
-                    <a href="app-notes.html" class="py-8 px-7 d-flex align-items-center">
-                      <span class="d-flex align-items-center justify-content-center bg-light rounded-1 p-6">
-                        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-tasks.svg" alt="" width="24" height="24">
-                      </span>
-                      <div class="w-75 d-inline-block v-middle ps-3">
-                        <h6 class="mb-1 bg-hover-primary fw-semibold">My Task</h6>
-                        <span class="d-block text-dark">To-do and Daily Tasks</span>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="d-grid py-4 px-7 pt-8">
-                    <div class="upgrade-plan bg-light-primary position-relative overflow-hidden rounded-4 p-4 mb-9">
-                      <div class="row">
-                        <div class="col-6">
-                          <h5 class="fs-4 mb-3 w-50 fw-semibold text-dark">Unlimited Access</h5>
-                          <button class="btn btn-primary text-white">Upgrade</button>
-                        </div>
-                        <div class="col-6">
-                          <div class="m-n4">
-                            <img src="../../dist/images/backgrounds/unlimited-bg.png" alt="" class="w-100">
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <a href="authentication-login.html" class="btn btn-outline-primary">Log Out</a>
+
+                    <a href="{{ route('front.logout') }}" class="btn btn-outline-primary">Log Out</a>
                   </div>
                 </div>
               </div>
