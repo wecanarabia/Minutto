ccc658940493a969328e4efb7db715daf311e740
mina <mina899432@gmail.com>
2023-09-21 16:38:58 +0300

    first

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,7 +8,7 @@
                   <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/backgrounds/errorimg.svg" alt="" class="img-fluid">
                   <h1 class="fw-semibold mb-7 fs-9">Opps!!!</h1>
                   <h4 class="fw-semibold mb-7">This page you are looking for could not be found.</h4>
-                  <a class="btn btn-primary" href="index.html" role="button">Go Back to Home</a>
+                  <a class="btn btn-primary" href="{{ route('front.home') }}" role="button">Go Back to Home</a>
                 </div>
               </div>
             </div>
