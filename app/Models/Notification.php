57301246e2abb3512ff53e5cd93bbb27bd366b5f
aya-matroud <94105930+aya-matroud@users.noreply.github.com>
2023-09-24 04:47:41 -0700

    Aya (#73)
    * test
    
    * addBasics
    
    * addCompany
    
    * fixed
    
    * addBranchAndShift
    
    * fixed
    
    * addUserAndMessage
    
    * fixed
    
    * addVacations
    
    * addLeaves
    
    * fixed
    
    * fixed
    
    * fixed
    
    * addchangepw
    
    * addWorkhours
    
    * addAlot
    
    * fixed
    
    * addReward
    
    * fixed
    
    * addExtra
    
    * updateLeave
    
    * updateLeave
    
    * fixed
    
    * fixed
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fixed
    
    * fixed
    
    * addUpdates
    
    * fixed
    
    * fixed
    
    * editLeave
    
    * fixed
    
    * addLastWorkhourApi
    
    * addApis
    
    * addGetLastLeave
    
    * addSalary
    
    * fixed
    
    * fixed
    
    * addAdvance
    
    * fixed
    
    * test
    
    * test
    
    * test
    
    * fixed
    
    * addSalaries
    
    * fixed
    
    * fixedleaves
    
    * fixed
    
    * fixed
    
    * fixed
    
    * addGetTimeByTimeZone
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * addCurrency
    
    * fixed
    
    * addTimezone
    
    * addUpdates
    
    * addIMEI
    
    * fixed
    
    * addUpdates
    
    * addPagesAndFaqs
    
    * testCronJob
    
    * test
    
    * fixed
    
    * fixed
    
    * fixed

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+
+    protected $guarded=[];
+
+    public function company(){
+        return $this->belongsTo(Company::class);
+    }
+
 }
